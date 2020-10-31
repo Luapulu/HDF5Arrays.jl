271f12dceb324b38469cb2ac6556b473de3fd4cb
Paul Nemec <paul.nemec@tum.de>
2020-10-31 15:29:04 +0100

    Simplified working version

--- src/HDF5Arrays.jl
+++ src/HDF5Arrays.jl
@@ -1,31 +1,26 @@
 module HDF5Arrays
 
-using DiskArrays: AbstractDiskArray, Unchunked, Chunked
 using HDF5:
     DataFile, HDF5Dataset, h5open, d_create, datatype, dataspace,
     get_chunk
-using ChainedArrays: ChainedVector
 
-import Base: size
-import DiskArrays: readblock!, writeblock!, haschunks, eachchunk
+import Base: size, getindex, setindex!
 
 export HDF5Array
 
-struct HDF5Array{T, N, D} <: AbstractDiskArray{T, N}
+struct HDF5Array{T, N, D} <: AbstractArray{T, N}
     data::D
-    chunked::Bool
 end
+
+const HDF5Vector{T, D} = HDF5Array{T, 1, D}
 
 size(arr::HDF5Array) = size(arr.data)
 
-readblock!(arr::HDF5Array, out, i::AbstractUnitRange...) = out .= arr.data[i...]
-writeblock!(arr::HDF5Array, X, i::AbstractUnitRange...) = arr.data[i...] = X
+getindex(arr::HDF5Array, I::Vararg{Integer, N}) where {N} =
+    getindex(arr.data, I...)
 
-haschunks(arr::HDF5Array{T, N, HDF5Dataset}) where {T, N} = arr.chunked ? Chunked() : Unchunked()
-
-eachchunk(arr::HDF5Array) = eachchunk(arr, haschunks(arr))
-eachchunk(arr::HDF5Array{<:Any, <:Any, HDF5Dataset}, ::Chunked) =
-    GridChunks(arr.data, get_chunk(arr.data))
+setindex!(arr::HDF5Array, v, I::Vararg{Integer, N}) where {N} =
+    setindex!(arr.data, v, I...)
 
 include("construct.jl")
 include("chained.jl")
--- src/construct.jl
+++ src/construct.jl
@@ -1,16 +1,3 @@
-
-
-function HDF5Array{T, N, D}(data) where {T, N, D<:HDF5Dataset}
-    try
-        get_chunk(data)
-        return HDF5Array{T, N, D}(data, true)
-    catch ErrorException
-        return HDF5Array{T, N, D}(data, false)
-    end
-end
-
-const HDF5Vector{T, D} = HDF5Array{T, 1, D}
-
 _tofile(f::AbstractString) = h5open(f, "r+")
 _tofile(f::DataFile) = f
 
@@ -22,12 +9,5 @@ function HDF5Array{T, N}(
     file::Union{AbstractString, DataFile}, path::AbstractString,
     ::UndefInitializer, dims::Vararg{Integer, N}
 ) where {T, N}
-    HDF5Array{T, N, HDF5Dataset}(d_create(_tofile(file), path, datatype(T), dataspace(dims)), false)
+    HDF5Array{T, N, HDF5Dataset}(d_create(_tofile(file), path, datatype(T), dataspace(dims)))
 end
-
-const HDF5Chain{T, CN} = ChainedVector{T, CN, HDF5Array{T, 1, D}} where D
-
-HDF5Array(chain::HDF5Chain{T}) where T = HDF5Vector{T, HDF5Chain{T}}(chain)
-HDF5Array(h5chain, path::AbstractString) = HDF5Array(
-    ChainedVector(map(f -> HDF5Array(f, path), h5chain)...)
-)
