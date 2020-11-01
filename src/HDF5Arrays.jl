812c821672c3895fab2ef9a033d8f91e147b73b0
Paul Nemec <58543214+Luapulu@users.noreply.github.com>
2020-11-01 21:15:10 +0100

    Chunked iteration and basics (#6)
    * Remove unneeded deps
    
    * Implement compression and chunking
    
    * Add TiledIteration
    
    * Add chunked iteration
    
    * Infer chunking from dataset
    
    * Test iteration and chunking
    
    * Remove chaining
    
    * Fix duped deps
    
    * Remove unused file
    
    * Add test for get_chunk with no chunking
    
    * Add HDF5 compat
    
    * Update julia compat to v1.3
    
    * Test on julia v1.3+
    
    * Replace tempname with mktemp

--- src/HDF5Arrays.jl
+++ src/HDF5Arrays.jl
@@ -1,13 +1,85 @@
 module HDF5Arrays
 
-using DiskArrays
-using HDF5: HDF5Dataset
+using HDF5: DataFile, HDF5Dataset, h5open, d_create, datatype, dataspace
+using TiledIteration: TileIterator
 
-import Base: size
-import DiskArrays: readblock!, writeblock!
+import Base: size, getindex, setindex!, iterate, to_indices, firstindex, lastindex
+import HDF5: get_chunk
 
 export HDF5Array
 
-include("HDF5Array.jl")
+struct HDF5Array{T, N, D, C<:Union{Nothing, NTuple{N, Int}}} <: AbstractArray{T, N}
+    data::D
+    chunk::C
+end
+
+const HDF5Vector{T, D, C} = HDF5Array{T, 1, D, C}
+
+size(arr::HDF5Array) = size(arr.data)
+
+getindex(arr::HDF5Array{T}, I::Integer...) where {T} = getindex(arr.data, I...)::T
+
+getindex(arr::HDF5Array{T}, I::Union{Colon, Integer, AbstractRange}...) where {T} =
+    getindex(arr.data, I...)::Array{T}
+
+setindex!(arr::HDF5Array, v, I::Union{Colon, Integer, AbstractRange}...) =
+    setindex!(arr.data, v, I...)
+
+# Read in one operation, rather than iterating index by index
+to_indices(arr::HDF5Array, I::Tuple{<:CartesianIndices}) = I[1].indices
+
+function iterate(arr::HDF5Array{T, N, HDF5Dataset, NTuple{N, Int}}) where {T, N}
+    # init chunk axes
+    cax_itr = TileIterator(axes(arr), arr.chunk)
+
+    @debug "Chunk axes itr: $(cax_itr)"
+
+    nextcax = iterate(cax_itr)
+    isnothing(nextcax) && return nothing
+    cax, cax_s = nextcax
+
+    @debug "First chunk axes: $(cax)"
+
+    buf::Array{T, N} = arr[cax...]
+
+    @debug "First buffer: $(buf)"
+
+    nextb = iterate(buf)
+    isnothing(nextb) && return nothing
+
+    b, b_s = nextb
+
+    return b, ((buf, b_s), (cax_itr, cax_s))
+end
+
+function iterate(arr::HDF5Array{T, N, HDF5Dataset, NTuple{N, Int}}, state) where {T, N}
+    (buf, b_s), (cax_itr, cax_s) = state
+
+    nextb = iterate(buf, b_s)
+
+    if isnothing(nextb)
+        # get next buffer
+        nextcax = iterate(cax_itr, cax_s)
+        isnothing(nextcax) && return nothing
+        cax, cax_s = nextcax
+
+        @debug "Next chunk axes: $(cax)"
+
+        buf::Array{T, N} = arr[cax...]
+
+        @debug "Next buffer: $(buf)"
+
+        nextb = iterate(buf)
+    end
+
+    b, b_s = nextb
+
+    return b, ((buf, b_s), (cax_itr, cax_s))
+end
+
+firstindex(arr::HDF5Array) = first(eachindex(arr))
+lastindex(arr::HDF5Array) = last(eachindex(arr))
+
+include("construct.jl")
 
 end
---
+++ src/construct.jl
@@ -0,0 +1,57 @@
+_tofile(f::AbstractString) = h5open(f, "r+")
+_tofile(f::DataFile) = f
+
+get_chunk(arr::HDF5Array{<:Any, <:Any, <:Any, Nothing}) = error("given HDF5Array is not chunked")
+get_chunk(arr::HDF5Array{<:Any, N, HDF5Dataset, NTuple{N, Int}}) where {N} = arr.chunk
+
+function HDF5Array(dset::HDF5Dataset)
+    try
+        c = get_chunk(dset)
+        return HDF5Array{eltype(dset), ndims(dset), HDF5Dataset, typeof(c)}(dset, c)
+    catch ErrorException
+        return HDF5Array{eltype(dset), ndims(dset), HDF5Dataset, Nothing}(dset)
+    end
+end
+HDF5Array(file::Union{AbstractString, DataFile}, path::AbstractString) =
+    HDF5Array(_tofile(file)[path])
+
+HDF5Array{T, N, D, Nothing}(data) where {T, N, D} = HDF5Array{T, N, D, Nothing}(data, nothing)
+
+_make_dim_args(
+    T::Type, dims::NTuple{N, Integer},
+    max_dims::NTuple{N, Integer}, chunk::NTuple{N, Integer}
+) where {N} = (T, (dims, max_dims), "chunk", chunk)
+
+_make_dim_args(
+    T::Type, dims::NTuple{N, Integer},
+    max_dims::Nothing, chunk::Nothing
+) where {N} = (datatype(T), dataspace(dims...))
+
+_make_dim_args(
+    T::Type, dims::NTuple{N, Integer},
+    max_dims::Nothing, chunk::NTuple{N, Integer}
+) where {N} = (datatype(T), dataspace(dims...), "chunk", chunk)
+
+_convert_chunk_type(c::NTuple{N, Integer}) where {N} = NTuple{N, Int}(c)
+_convert_chunk_type(::Nothing) = nothing
+
+_make_filter_args(compress::Integer) = ("compress", compress)
+_make_filter_args(::Nothing) = ()
+
+function HDF5Array{T, N}(
+    file::Union{AbstractString, DataFile}, path::AbstractString,
+    ::UndefInitializer, dims::Vararg{Integer, N};
+    chunk::Union{NTuple{N, Integer}, Nothing} = nothing,
+    max_dims::Union{NTuple{N, Integer}, Nothing} = nothing,
+    compress::Union{Integer, Nothing} = nothing
+) where {T, N}
+    c = _convert_chunk_type(chunk)
+    return HDF5Array{T, N, HDF5Dataset, typeof(c)}(
+        d_create(
+            _tofile(file), path,
+            _make_dim_args(T, dims, max_dims, chunk)...,
+            _make_filter_args(compress)...
+        ),
+        c
+    )
+end
